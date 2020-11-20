723c7bcaf7485c021daf9ea61efe657410403b18
Gyobeom <ikiscc@gmail.com>
2020-11-20 19:27:49 +0900

    수정

---
+++ main1.go
@@ -0,0 +1,32 @@
+3ckage main
+
+import (
+	        "fmt"
+		        "log"
+			        "../datafile"
+				        )
+					func main(){
+						        lines, err := datafile.GetStrings("votes.txt")
+							        if err != nil {
+									        log.Fatal(err)
+										      }
+										            var names []string
+											          var counts []int
+												        for _, line := range lines {
+														              matched := false
+															                    for i, name := rand names{
+																		                          if name == line{
+																						                                count[i]++
+																										                              matched = true
+																													                            }
+																																                  }
+																																		                if matched == false {
+																																					                      names = append(names, line)
+																																							                            counts = append(count, 1)
+																																										                  }
+																																												          }
+																																													          for i,name := range names{
+																																															                  fmt.Printf("%s: %d\n", name,counts[i])
+																																																	          }
+																																																	  }                                                                               ~                                                                                        ~                                                                                        ~                                                                                        ~        
+																																																	  ~                                                                                        ~                             
