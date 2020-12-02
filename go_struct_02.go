e325b2e76bd448f68f9a5b86743340021c610f98
Gyobeom <ikiscc@gmail.com>
2020-12-02 21:33:49 +0900

    struct

---
+++ go_struct_02.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type subscriber struct {
+	name string
+	rate int
+	active bool
+}
+func main(){
+	var s1 subscriber
+
+	s1.name = "kim"
+	s1.rate = 5000
+	s1.active = false
+
+	fmt.Printf("%s\n",s1.name)
+	fmt.Println(s1.rate)
+	fmt.Println(s1.active)
+}
+
